b0a02914fb0194498d404c936ec756309a856033
Ting Zhu <me@merlinz.me>
2020-04-27 08:27:11 +0200

    update

--- main.js
+++ main.js
@@ -15,10 +15,11 @@ JSB.newAddon = function(mainPath) {
         },
         //MARK: MN behaviors
         notebookWillOpen: function(notebookid) {
-
+            NSNotificationCenter.defaultCenter().addObserverSelectorName(self, 'onTextSelection:', 'TextSelection');
+            self.instantCopy = NSUserDefaults.standardUserDefaults().objectForKey('marginnote_instantcopy');
         },
         notebookWillClose: function(notebookid) {
-
+            NSNotificationCenter.defaultCenter().removeObserverName(self, 'TextSelection');
         },
         documentDidOpen: function(docmd5) {
 
@@ -30,15 +31,42 @@ JSB.newAddon = function(mainPath) {
 
         },
         queryAddonCommandStatus: function() {
-
+            if(Application.sharedInstance().studyController(self.window).studyMode < 3) {
+                return {
+                    image: 'instantcopy.png',
+                    object: self,
+                    selector: "toggleInstantCopy:",
+                    checked: (self.instantCopy?true:false)
+                };
+            }
+            return null;
         },
 
         //TODO: Selecting text on document
-
+        onTextSelection: function(sender) {
+            if(!Application.sharedInstance().checkNotifySenderInWindow(sender, self.window))
+                return; // Don't process message from other window
+            if(!self.instantCopy)
+                return;
+            var text = sender.userInfo.documentController.selectionText;
+            if(text && text.length) {
+                // Copy to Clipboard
+                self.instantCopy = !self.instantCopy;
+            }
+        },
 
         //TODO: copy text to Clipboard while selecting text
-
-
+        toggleInstantCopy: function(sender) {
+            if(self.instantCopy) {
+                self.instantCopy = false;
+                Application.sharedInstance().showHUD('自动复制关闭', self.window, 2);
+            } else {
+                self.instantCopy = true;
+                Application.sharedInstance().showHUD('自动复制打开', self.window, 2);
+            }
+            NSUserDefaults.standardUserDefaults().setObjectForKey(self.instantCopy, 'marginnote_instantcopy');
+            Application.sharedInstance().studyController(self.window).refreshAddonCommands();
+        },
 
     }, {
         //MARK: - Class Method Definitions
